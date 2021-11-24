d7a5030bd26b19f562c968995f550e08e1783156
paulihaarnio <pauli.haarnio@gmail.com>
2021-11-24 10:35:59 +0200

    js

---
+++ js/pauli.js
@@ -0,0 +1,13 @@
+
+
+document.querySelector("button").addEventListener("click", check)
+
+function check(){
+    let vastaus=Number(document.getElementById("vastaus").value)
+    if(vastaus===15){
+        document.getElementById("vastausKenttä").innerText="Oikea vastaus, hienoa!"
+    }
+    else{
+        document.getElementById("vastausKenttä").innerText="Väärä vastaus, yritä uudelleen."
+    }
+}
