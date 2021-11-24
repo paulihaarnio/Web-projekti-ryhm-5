0f1be66647f3c931b792ebd2c9af6834d462212c
rikulis <remesrikua@gmail.com>
2021-11-24 09:54:09 +0200

    kuvia

---
+++ js/Riku.js
@@ -0,0 +1,4 @@
+ function vaihto() {
+    document.getElementById("kuva").innerHTML='<img src="../" alt="Girl in a jacket"  width="637px" height="300px">'
+    document.getElementById("teksti").innerHTML="Minkä maan lippu on kuvassa"
+ } 
