1182f6e1eff3ee4995071f02bd6a12478a79ffc9
booennm <booennm@gmail.com>
2021-12-07 14:40:29 +0200

    vastauksen tarkistus toimii, bugi kahessa viimisessä

--- js/petovisa.js
+++ js/petovisa.js
@@ -15,7 +15,8 @@ let printedQuestion = 0;
 class Question {
     question = "";
     img = "";
-    answers = []; //the correct answer is always first in this array
+    answers = [];
+    correctAnswer = "";
     result = false;
 
     constructor(q, img) {
@@ -39,6 +40,10 @@ class Question {
         return this.result;
     }
 
+    getCorrectAnswer() {
+        return this.correctAnswer;
+    }
+
     setQuestion(q) {
         this.question = q;
     }
@@ -55,16 +60,30 @@ class Question {
         this.result = boolean;
     }
 
+    setCorrectAnswer(string) {
+        this.correctAnswer = string;
+    }
+
     
     printQuestion() {
         document.getElementById("question").textContent = this.question;
         document.getElementById("questionImg").src = this.img;
 
         let div = document.getElementById("answers");
-        let answers = div.querySelectorAll("label");
+        let labels = div.querySelectorAll("label");
 
-        for(let i=0; i<answers.length; i++) {
-            answers[i].textContent = this.answers[i];
+        let shuffeledAnswers = this.answers;
+
+        shuffeledAnswers.sort(() => Math.random() - 0.5); //https://javascript.info/task/shuffle
+
+        for(let i=0; i<labels.length; i++) {
+
+            labels[i].textContent = shuffeledAnswers[i];
+
+            if(shuffeledAnswers[i] == this.correctAnswer) {
+                let input = document.getElementById("answer" + (i+1));
+                input.classList.add("correct");
+            }
         }
     }
 }
@@ -78,6 +97,7 @@ answers0[1] = "Supikoira";
 answers0[2] = "Karhu";
 answers0[3] = "Susi";
 questions[0].setAnswers(answers0);
+questions[0].setCorrectAnswer("Ahma");
 
 questions[1] = new Question("Mikä väittämistä EI OLE totta?", "#");
 let answers1 = [];
@@ -86,6 +106,7 @@ answers1[1] = "Karhut nukkuvat talviunta";
 answers1[2] = "Karhut ovat kaikkiruokaisia";
 answers1[3] = "Karhu on Suomen suurin petoeläin";
 questions[1].setAnswers(answers1);
+questions[1].setCorrectAnswer("Karhut ovat laumaeläimiä");
 
 questions[2] = new Question("Mikä petoeläin on kuvassa", "../kuvat/kärppä.jpg");
 let answers2 = [];
@@ -94,6 +115,7 @@ answers2[1] = "Lumikko";
 answers2[2] = "Mäyrä";
 answers2[3] = "Ilves";
 questions[2].setAnswers(answers2);
+questions[2].setCorrectAnswer("Kärppä");
 
 questions[3] = new Question("Minkä petoeläimen jäljet ovat kuvassa?", "../kuvat/kettu.jpg");
 let answers3 = [];
@@ -102,6 +124,7 @@ answers3[1] = "Saukko";
 answers3[2] = "Susi";
 answers3[3] = "Ilves";
 questions[3].setAnswers(answers3);
+questions[3].setCorrectAnswer("Kettu");
 
 questions[4] = new Question("Miksi ilveksellä on leveät tassut?", "#");
 let answers4 = [];
@@ -110,14 +133,26 @@ answers4[1] = "Niillä on helpompi metsästää";
 answers4[2] = "Paksujen kynsien vuoksi";
 answers4[3] = "Ne helpottavat puuhun kiipeämistä";
 questions[4].setAnswers(answers4);
+questions[4].setCorrectAnswer("Ne helpottavat lumen päällä kävelyä");
 
 document.querySelector("#submit").addEventListener("click", submitAnswer);
 document.querySelector("#next").addEventListener("click", nextQuestion);
 
 questions[0].printQuestion();
 
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
 function submitAnswer() {
-    document.getElementById("result").textContent = "vastattu";
+    let answer = document.querySelector("input[name='answer']:checked");
+
+    if(answer.classList.contains("correct")) {
+        document.getElementById("result").innerHTML = "Oikein!";
+    }else {
+        document.getElementById("result").innerHTML = "Väärin";
+    }
 }
 
 function nextQuestion() {
