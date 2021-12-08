1c84905da903366f7b72c895fccc253057dbbe78
booennm <booennm@gmail.com>
2021-12-08 14:10:42 +0200

    toimii hyvin, tulosruutua aloteltu

--- js/petovisa.js
+++ js/petovisa.js
@@ -1,23 +1,12 @@
-/**
- * Question-class jossa:
- * kysymys
- * kuva
- * vastaukset
- * tulos true or false
- * 
- * tarkista vastaus
- * 
- * printtaa kysymys
- * arvo vastausten järjestys
- */
 let printedQuestion = 0;
+
+let results = [];
 
 class Question {
     question = "";
     img = "";
     answers = [];
     correctAnswer = "";
-    result = false;
 
     constructor(q, img) {
         this.question = q;
@@ -63,9 +52,16 @@ class Question {
     setCorrectAnswer(string) {
         this.correctAnswer = string;
     }
-
     
     printQuestion() {
+        let submitButton = document.getElementById("submit");
+        let nextButton = document.getElementById("next");
+
+        submitButton.removeAttribute("disabled", "");
+        nextButton.setAttribute("disabled", "");
+
+        document.getElementById("result").textContent = "";
+
         document.getElementById("question").textContent = this.question;
         document.getElementById("questionImg").src = this.img;
 
@@ -80,11 +76,23 @@ class Question {
 
             labels[i].textContent = shuffeledAnswers[i];
 
+            let input = document.getElementById("answer" + (i+1));
+
             if(shuffeledAnswers[i] == this.correctAnswer) {
-                let input = document.getElementById("answer" + (i+1));
                 input.classList.add("correct");
+            }else {
+                input.classList.remove("correct");
+                input.classList.add("incorrect");
             }
         }
+    }
+
+    printCorrectAnswer() {
+        let p = document.createElement("p");
+
+        p.textContent = this.question + "<br>Oikea vastaus on: " + this.correctAnswer;
+
+        return p;
     }
 }
 
@@ -150,14 +158,71 @@ function submitAnswer() {
 
     if(answer.classList.contains("correct")) {
         document.getElementById("result").innerHTML = "Oikein!";
+        results[printedQuestion] = true;
     }else {
         document.getElementById("result").innerHTML = "Väärin";
+        results[printedQuestion] = false;
     }
+
+    let submitButton = document.getElementById("submit");
+    let nextButton = document.getElementById("next");
+
+    submitButton.setAttribute("disabled", "");
+    nextButton.removeAttribute("disabled", "");
 }
 
 function nextQuestion() {
-    printedQuestion++;
-    questions[printedQuestion].printQuestion();
+    if(printedQuestion == 4) {
+        printResults();
+    }else {
+        printedQuestion++;
+        questions[printedQuestion].printQuestion();
+    }
+    
 }
 
-//tulos sivun printtaus
+function printResults() {
+
+    let div = document.getElementById("quiz");
+
+    div.innerHTML = "";
+
+    let h2 = document.createElement("h2");
+    h2.textContent = "Visa on ohi"
+
+    let h1 = document.createElement("h1");
+    h1.classList.add("result");
+
+    let correct = 0;
+    let all = 0;
+    results.forEach(result => {
+        all++;
+        if(result == true) {
+            correct++;
+        }
+    });
+
+    h1.textContent = correct + "/" + all;
+
+    let h22 = document.createElement("h2");
+    h22.textContent = "vastausta oikein";
+
+    let button = document.createElement("button");
+    button.textContent = "Yritä uudelleen";
+    button.addEventListener("click", location.reload);
+
+    div.append(h2, h1, h22, button);
+
+    let content = document.getElementById("content");
+
+    let col = document.createElement("div");
+    col.classList.add("col");
+
+    let header = document.createElement("h1");
+    header.textContent = "Vastausten erittely";
+
+    
+
+    col.append(header);
+    content.appendChild(col);
+}
